f7aa633f3ac4f5d7e0b7a21d3ffdc9e5b4bae010
inhyeokJeon <poiu2389@naver.com>
2021-07-15 18:14:25 +0900

    board

---
+++ AALGGO/BOARDCOVER.cpp
@@ -0,0 +1,16 @@
+//
+//  BOARDCOVER.cpp
+//  AALGGO
+//
+//  Created by inhyeok on 2021/07/15.
+//
+
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
