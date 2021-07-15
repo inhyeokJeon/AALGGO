81bbb0b8eefeec8445420c6f64765d8d76b715b0
inhyeokJeon <poiu2389@naver.com>
2021-07-15 14:38:15 +0900

    Initial Commit

---
+++ AALGGO/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AALGGO
+//
+//  Created by inhyeok on 2021/07/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
