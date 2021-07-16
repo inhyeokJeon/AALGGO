0c56fb16ccff83af1700154130f66e6d84b280ab
inhyeokJeon <poiu2389@naver.com>
2021-07-16 22:01:22 +0900

    7-16

---
+++ AALGGO/brute/clocksync.cpp
@@ -0,0 +1,77 @@
+//
+//  clocksync.cpp
+//  AALGGO
+//
+//  Created by inhyeok on 2021/07/16.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const int INF = 9999;
+
+ifstream fin("test_clock.txt");
+int clock_list[10][16] = {
+    {1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0}, //0
+    {0,0,0,1,0,0,0,1,0,1,0,1,0,0,0,0},//1
+    {0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1},//2
+    {1,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0},//3
+    {0,0,0,0,0,0,1,1,1,0,1,0,1,0,0,0},
+    {1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1}, // 5
+    {0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,1},
+    {0,0,0,0,1,1,0,1,0,0,0,0,0,0,1,1},// 7
+    {0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,1,1,1,0,0,0,1,0,0,0,1,0,0} //9
+};
+
+void push(vector<int> &table, int button){
+    for(int i=0; i<16; i++){
+        if(clock_list[button][i]==1){
+            table[i] += 3;
+            if(table[i] == 15){
+                table[i] = 3;
+            }
+        }
+    }
+}
+bool areAligned(const vector<int> & table){
+    for(int i=0; i< 16; i++){
+        if(table[i] != 12){
+            return false;
+        }
+    }
+    return true;
+}
+int clock(vector<int> &table, int button){
+    if(button == 10){
+        return areAligned(table) ? 0 : INF;
+    }
+    int ret = INF;
+    for( int i=0; i<4; i++){
+        ret = min(ret, i + clock(table, button+1));
+        push(table,button);
+    }
+    
+    return ret;
+}
+int main(int argc, const char * argv[]) {
+    int Test_case;
+    fin >> Test_case;
+    for (int i=0; i< Test_case ; i++){
+        vector<int> arr(16, 0);
+        for(int j = 0 ; j< 16; j++){
+            fin >> arr[j];
+            
+        }
+        if(clock(arr,0) == 9999){
+            cout << -1 << endl;
+        }
+        else{
+            cout << clock(arr,0) << endl;
+        }
+        
+    }
+    return 0;
+}
--- AALGGO/main.cpp
+++ AALGGO/main.cpp
@@ -1,15 +1,38 @@
 //
 //  main.cpp
 //  AALGGO
-//
+// next_permutation
 //  Created by inhyeok on 2021/07/15.
 //
 
 #include <iostream>
-#include <fstream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    vector <int> a;
+    a.push_back(7);
+    a.push_back(5);
+    a.push_back(2);
+    a.push_back(1);
+    a.push_back(10);
+    int arr[5] = {1,2,3,5,2};
+    //fill(a.begin(), a.end(), 0);
+    sort(a.begin(), a.end(), true);
+    /*
+    do {
+        for(int i=0; i < a.size(); i++){
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    } while(prev_permutation(a.begin(), a.end()));
+    */
+    for(int i=0; i< 5; i++){
+        cout << a[i] << " ";
+    }
     return 0;
 }
