68d1c729d1ea62892308914f57824c998a7fd4f0
inhyeokJeon <poiu2389@naver.com>
2022-09-02 13:41:39 +0900

    sort 973

--- Python/LeetCode/sort/973_k_closest_point.py
+++ Python/LeetCode/sort/973_k_closest_point.py
@@ -3,9 +3,6 @@ from typing import *
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        def sort_algorithm(l1, l2) -> bool:
-            return l1[0]**2 + l1[1]**2 < l2[0]**2 + l2[1]**2
-
         points.sort(key=lambda x: x[0]**2 + x[1]**2)
         return points[:k]
 
