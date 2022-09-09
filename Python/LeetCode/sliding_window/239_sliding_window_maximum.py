5da6af1aeb2e3cebf78868acbd608971db4ebfbf
inhyeokJeon <poiu2389@naver.com>
2022-09-09 23:20:07 +0900

    sliding window

--- Python/LeetCode/sliding_window/239_sliding_window_maximum.py
+++ Python/LeetCode/sliding_window/239_sliding_window_maximum.py
@@ -1,15 +1,27 @@
 from typing import List
-
+import collections
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         # brute force -> time limit exceed
+        # result = []
+        # for i in range(len(nums) - k + 1):
+        #     result.append(max(nums[i:i+k]))
+        # return result
         result = []
-        for i in range(len(nums) - k + 1):
-            result.append(max(nums[i:i+k]))
+        window = collections.deque()
+        for i, v in enumerate(nums):
+            while window and window[-1] < v:
+                window.pop()
+            window.append(v)
+            if i < k - 1:
+                continue
+            else:
+                result.append(window[0])
+                if nums[i - k + 1] == window[0]:
+                    window.popleft()
         return result
-        # for i, v in enumerate(nums):
 
 nums = [1,3,-1,-3,5,3,6,7]
-sol = Solution(),
+sol = Solution()
 print(sol.maxSlidingWindow(nums, 3))
